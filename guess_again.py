5216e8c4abe0267fd8842fd46e6e8cb7ac72384f
reese <reesedporter@gmail.com>
2021-03-24 13:55:26 -0500

    print guesses

--- guess_again.py
+++ guess_again.py
@@ -1,14 +1,19 @@
 import random
+import inflect
 
 def main():
+    p = inflect.engine()
+
     one = 1
     seven_hundred_billion = 700_000_000_000
 
     im_thinking_of_a_number_between = random.randint(one, seven_hundred_billion)
+    print('I\'m thinking of a number between one and seven hundred billion. Try to guess it.')
 
     try_to_guess_it = True
     while try_to_guess_it:
         guess = random.randint(one, seven_hundred_billion)
+        print(p.number_to_words(guess))
         try_to_guess_it = (guess != im_thinking_of_a_number_between)
         if try_to_guess_it:
             print('Nope. Guess again.')
